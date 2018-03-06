aaa7fe8c39a19322352e7e3e4c31abb1b409a43c
Jimmy Van <jvan915@uw.edu>
2018-03-05 19:01:38 -0800

    Finished Graph for USA All Jobs

--- Wage_Demographic_Comparison/ui.R
+++ Wage_Demographic_Comparison/ui.R
@@ -22,14 +22,14 @@ shinyUI(fluidPage(
   # Pick job Title
   sidebarLayout(
     sidebarPanel(
-      radioButtons("compare", label = h3("Compare Wage By"), choices = list("All Jobs" = 1, "Two different Jobs" = 2), selected = 1), br(),
-      selectInput('catagory1', 'Compare By', colnames(seattle_data[,2:11]), selected = "Female.Avg.Hrly.Rate"),
-      selectInput('catagory2', 'And', colnames(seattle_data[,2:11]), selected = "No..Female.Empl"), 
+      radioButtons("compare", label = h3("Compare By"), choices = list("All Jobs" = 1, "Specific Jobs" = 2), selected = 1), br(),
+      selectInput('catagory', 'Compare By', c("Wage","Ratio of Men and Women"), selected = "Wage"),
       selectInput('job1', 'Pick A Job', seattle_data[,1], selected = "Accountant"),
-      selectInput('job2', 'Pick A Job', seattle_data[,1], selected = "Act Exec"),
       hr(),
       helpText(paste0("Note: If the graph is negative 1, this means we do not have data on that job"))
     ),
+    
+    
       # Show a plot of the generated distribution
     mainPanel(
        plotOutput("distPlot")
