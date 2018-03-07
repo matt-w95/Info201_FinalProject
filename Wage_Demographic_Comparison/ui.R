0cdbd3a64fb9984b13cbe2c74f79aa6632b49791
Hannah <chungh16@uw.edu>
2018-03-06 18:10:49 -0800

    Edited Side Panel to match Tabs

--- Wage_Demographic_Comparison/ui.R
+++ Wage_Demographic_Comparison/ui.R
@@ -17,16 +17,18 @@ shinyUI(fluidPage(
   # Pick job Title
   sidebarLayout(
     sidebarPanel(
+      helpText("Click the 'Top 10' tab"),
       selectInput("choose_gender_best_city", "Choose the gender",
                   choices = c("Male", "Female")),
+      hr(),
       
-      radioButtons("compare", label = h3("Compare By"), choices = list("All Jobs" = 1, "Specific Jobs" = 2), selected = 1),
+      helpText("Click the 'General Trend VS Individual' tab"),
+      radioButtons("compare", label = "Compare By", choices = list("All Jobs" = 1, "Specific Jobs" = 2), selected = 1),
       conditionalPanel(
         condition = "input.compare == 2",
         selectInput('job1', 'Pick A Job', usa_data[,which(colnames(usa_data)== "soc_name" )], selected = "Accountant")
       ),
-      hr(),
-      helpText(paste("Note:","The USA data is from 2015", sep="\n"))
+      helpText("Note: The USA data is from 2015")
       
     ),
     
