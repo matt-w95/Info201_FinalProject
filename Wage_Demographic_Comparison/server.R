6615b1218e22b77cccab761c14a931144a4addb6
matt-w95 <matt.wong95@gmail.com>
2018-03-07 14:32:44 -0800

    Added Seattle graph for general trends/ specfic jobs

--- Wage_Demographic_Comparison/server.R
+++ Wage_Demographic_Comparison/server.R
@@ -8,7 +8,7 @@ library("reshape")
 shinyServer(function(input, output) {
   
   # Seattle data filtering
-  Seattle_GenderWage <- read.csv("~/Desktop/INFO201/Info201_FinalProject/Wage_Demographic_Comparison/data/City_of_Seattle_WageGender.csv") 
+  Seattle_GenderWage <- read.csv("./data/City_of_Seattle_WageGender.csv") 
   
   Seattle_withDiff <- mutate(Seattle_GenderWage, Male_Female_diff = (Seattle_GenderWage$Male.Avg.Hrly.Rate - Seattle_GenderWage$Female.Avg.Hrly.Rate))
   Seattle_withDiff <- na.omit(Seattle_withDiff)
@@ -30,8 +30,19 @@ shinyServer(function(input, output) {
   male_sub <- melt(male_sub, id=c("Jobtitle"))
   female_sub <- melt(female_sub, id=c("Jobtitle"))
   
+  #General Seattle data manipulation
+  Seattle_GenderWage_general <- na.omit(Seattle_GenderWage)
+  
+  worker_count_seattle <-
+    Seattle_GenderWage_general %>%
+    select(No..Female.Empl, No..Male.Empl)
+  
+  wage_seattle <-
+    Seattle_GenderWage_general %>%
+    select(Female.Avg.Hrly.Rate, Male.Avg.Hrly.Rate, Jobtitle)
+  
   # USA data Manipulation
-  usa_data <- read.csv(file='~/Desktop/INFO201/Info201_FinalProject/Wage_Demographic_Comparison/data/United_States_WageGender.csv', sep=",", header=TRUE)
+  usa_data <- read.csv(file='./data/United_States_WageGender.csv', sep=",", header=TRUE)
   usa_data <- usa_data[usa_data$year == '2015',]
   usa_data <- usa_data[ -c(1:3,5,7,9, 11) ]
   usa_male <- usa_data[usa_data$sex_name == 'Male',]
@@ -101,27 +112,54 @@ shinyServer(function(input, output) {
   
   # Plots general trend of USA jobs or female VS male pay rates of individual jobs
   output$GeneralVSIndividual <- renderPlot({
-    if(input$compare == 1){
-      usa_ratio <- ggplot(usa_data, aes(x=usa_data$female_num_ppl , y=usa_data$male_num_ppl)) + geom_point() + 
-        xlab("# of Female Workers") + ylab("# of Male Workers") + ggtitle("Ratios of Male to Female Workers (USA)") +
-        xlim(0, 3500000) + ylim(0,3500000) + geom_abline(intercept = 0, slope = 1)
-      usa_avg_wage <- ggplot(usa_data, aes(x=usa_data$female_avg_wage_ft, y=usa_data$female_num_ppl))+ geom_point()+ 
-        xlab("Wage of Female Workers") + ylab("Wage of Male Workers") + ggtitle("Ratios of Male to Female Wages (USA)") + 
-        geom_abline(intercept = 0, slope = 1)
-    }else{
-      xaxis <-c("Female","Male")
-      yratio <-c(usa_data[which(usa_data == input$job1),2], usa_data[which(usa_data == input$job1),4])
-      ratio_temp <- data.frame(sex=xaxis,workers=yratio)
-      usa_ratio <- ggplot(ratio_temp, aes(x=sex, y=workers, fill=c("blue","pink"))) + geom_bar(stat = "identity") +
-        theme(legend.position="none") + geom_text(aes(label=workers), vjust=1.6, color="white", size=3.5) +
-        ylab("# Workers") + ggtitle("Ratios of Male to Female Workers (USA)") 
-      ywage <-c(usa_data[which(usa_data == input$job1),3], usa_data[which(usa_data == input$job1),5]) 
-      wage_temp <- data.frame(sex=xaxis,Wages=ywage)
-      usa_avg_wage <- ggplot(wage_temp, aes(x=sex, y=Wages, fill=c("blue","pink"))) + geom_bar(stat = "identity") +
-        theme(legend.position="none" ) + geom_text(aes(label=Wages), vjust=1.6, color="white", size=3.5) +
-        ylab("Wage of Workers") + ggtitle("Ratios of Male to Female Wages (USA)")
-    }
-    ggarrange(usa_ratio, usa_avg_wage)
+      if(input$choose_area_general == 3){
+        if(input$compare == 1){
+          usa_ratio <- ggplot(usa_data, aes(x=usa_data$female_num_ppl , y=usa_data$male_num_ppl)) + geom_point() + 
+            xlab("# of Female Workers") + ylab("# of Male Workers") + ggtitle("Ratios of Male to Female Workers (USA)") +
+            xlim(0, 3500000) + ylim(0,3500000) + geom_abline(intercept = 0, slope = 1)
+          usa_avg_wage <- ggplot(usa_data, aes(x=usa_data$female_avg_wage_ft, y=usa_data$female_num_ppl))+ geom_point()+ 
+            xlab("Wage of Female Workers") + ylab("Wage of Male Workers") + ggtitle("Ratios of Male to Female Wages (USA)") + 
+            geom_abline(intercept = 0, slope = 1)
+        }else{
+          xaxis <-c("Female","Male")
+          yratio <-c(usa_data[which(usa_data == input$job1),2], usa_data[which(usa_data == input$job1),4])
+          ratio_temp <- data.frame(sex=xaxis,workers=yratio)
+          usa_ratio <- ggplot(ratio_temp, aes(x=sex, y=workers, fill=c("blue","pink"))) + geom_bar(stat = "identity") +
+            theme(legend.position="none") + geom_text(aes(label=workers), vjust=1.6, color="white", size=3.5) +
+            ylab("# Workers") + ggtitle("Ratios of Male to Female Workers (USA)") 
+          ywage <-c(usa_data[which(usa_data == input$job1),3], usa_data[which(usa_data == input$job1),5]) 
+          wage_temp <- data.frame(sex=xaxis,Wages=ywage)
+          usa_avg_wage <- ggplot(wage_temp, aes(x=sex, y=Wages, fill=c("blue","pink"))) + geom_bar(stat = "identity") +
+            theme(legend.position="none" ) + geom_text(aes(label=Wages), vjust=1.6, color="white", size=3.5) +
+            ylab("Wage of Workers") + ggtitle("Ratios of Male to Female Wages (USA)")
+        }
+        ggarrange(usa_ratio, usa_avg_wage)
+      } else if(input$choose_area_general == 4) {
+        if(input$compare == 1){
+          seattle_ratio <- ggplot(worker_count_seattle, aes(worker_count_seattle$No..Female.Empl, worker_count_seattle$No..Male.Empl)) + geom_point() + xlim(0,150) + ylim(0,150)
+          seattle_ratio <- seattle_ratio + geom_abline() + labs(x="Number of female employees", y="Number of male employees", title="General trend of counts of male and female employees in Seattle")
+          
+          seattle_wage <- ggplot(wage_seattle, aes(wage_seattle$Female.Avg.Hrly.Rate, wage_seattle$Male.Avg.Hrly.Rate)) + geom_point()
+          seattle_wage <- seattle_wage + geom_abline() + labs(x="Average female hourly wage", y = "Average male hourly wage", title = "General trend of male and female hourly wage in Seattle")
+        } else {
+          xaxis <-c("Female","Male")
+          yratio <-c(Seattle_GenderWage_general[which(Seattle_GenderWage_general == input$job2),3], Seattle_GenderWage_general[which(Seattle_GenderWage_general == input$job2),6])
+          
+          ratio_temp <- data.frame(sex=xaxis,workers=yratio)
+          
+          seattle_ratio <- ggplot(ratio_temp, aes(x=sex, y=workers, fill=c("blue","pink"))) + geom_bar(stat = "identity") +
+            theme(legend.position="none") + geom_text(aes(label=workers), vjust=1.6, color="white", size=3.5) +
+            ylab("# Workers") + ggtitle("Ratios of Male to Female Workers (Seattle)") 
+          
+          ywage <-c(Seattle_GenderWage_general[which(Seattle_GenderWage_general == input$job2),2], Seattle_GenderWage_general[which(Seattle_GenderWage_general == input$job2),5]) 
+          wage_temp <- data.frame(sex=xaxis,Wages=ywage)
+          
+          seattle_wage <- ggplot(wage_temp, aes(x=sex, y=Wages, fill=c("blue","pink"))) + geom_bar(stat = "identity") +
+            theme(legend.position="none" ) + geom_text(aes(label=Wages), vjust=1.6, color="white", size=3.5) +
+            ylab("Wage of Workers") + ggtitle("Ratios of Male to Female Wages (Seattle)")
+        }
+        ggarrange(seattle_ratio, seattle_wage)
+      }
     })
   
   # Sidepanels for plots
@@ -130,9 +168,14 @@ shinyServer(function(input, output) {
       sidePanel <- list(selectInput("choose_gender_best_city", "Choose the gender", choices = c("Male", "Female")))
     } else if (input$tab == "General Trend VS Individual") {
       sidePanel <- list(radioButtons("compare", label = "Compare By", choices = list("All Jobs" = 1, "Specific Jobs" = 2), selected = 1),
+                        radioButtons("choose_area_general", label = "Choose area", choices = list("USA" = 3, "Seattle" = 4), selected = 3),
              conditionalPanel(
-               condition = "input.compare == 2",
-               selectInput('job1', 'Pick A Job', usa_data[,which(colnames(usa_data)== "soc_name" )], selected = "Accountant")
+               condition = ("input.compare == 2 & input.choose_area_general == 3"),
+                  selectInput('job1', 'Pick A Job', usa_data[,which(colnames(usa_data)== "soc_name" )], selected = "Accountant")
+             ),
+             conditionalPanel(
+               condition = ("input.compare == 2 & input.choose_area_general == 4"),
+               selectInput('job2', 'Pick A Job', Seattle_GenderWage_general[,which(colnames(Seattle_GenderWage_general)== "Jobtitle" )], selected = "Accountant")
              ))
     }
     return(sidePanel)
